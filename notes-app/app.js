d05f782ec38428c296b504896ff4f8339724c2db
Gustavo <yecid.x.romero.-nd@disney.com>
2020-06-10 12:22:00 -0500

    Video 12 Printing the ChalkJs Color Library

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,13 @@
+const chalk = require('chalk');
 const validator = require('validator');
 const getNotes = require('./notes.js');
 
 const notes = getNotes();
+const yellow = chalk.bold.yellow;
+const red = chalk.bold.red;
+
 
 console.log(notes);
 console.log("isURL?", validator.isURL('https/mead.io'));
 console.log("isEmail?", validator.isEmail('bla@example.com'));
+console.log(`Welcome to ${yellow('Node JS')} with ${red('Chalk')}!`);
