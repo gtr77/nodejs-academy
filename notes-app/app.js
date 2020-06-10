6ec1f79b0c7025c84fb5fd616c38d5dfa1b19f2c
Gustavo <yecid.x.romero.-nd@disney.com>
2020-06-10 10:45:05 -0500

    Video 11 Importing npm modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,8 @@
+const validator = require('validator');
 const getNotes = require('./notes.js');
 
 const notes = getNotes();
 
-console.log("App", notes);
+console.log(notes);
+console.log("isURL?", validator.isURL('https/mead.io'));
+console.log("isEmail?", validator.isEmail('bla@example.com'));
