4c1e28c10cc6ded44f2d0d57bb915500fa3d6c62
Gustavo <yecid.x.romero.-nd@disney.com>
2020-06-10 16:23:42 -0500

    Video 13 Global Modules with nodemon

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,11 +3,19 @@ const validator = require('validator');
 const getNotes = require('./notes.js');
 
 const notes = getNotes();
-const yellow = chalk.bold.yellow;
-const red = chalk.bold.red;
+const green = chalk.green.inverse.bold;
+const blue = chalk.blue.inverse.bold;
+const red = chalk.red.inverse.bold;
 
 
 console.log(notes);
 console.log("isURL?", validator.isURL('https/mead.io'));
 console.log("isEmail?", validator.isEmail('bla@example.com'));
-console.log(`Welcome to ${yellow('Node JS')} with ${red('Chalk')}!`);
+console.log(`Welcome to ${red('Node JS')} with ${blue('Chalk')}!`);
+
+// Installing Nodemon in our App
+// Run npm i -g nodemon ----> to install globally
+// Instead node app.js ---> Use nodemon app.js
+// With nodemon you can save changes and without re-run the app see the changes directly
+
+// NODEMON WORKS WITHOUT RUN IT AGAIN
