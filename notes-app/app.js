afb49804f03bd063555ffec09741a183d2e0c1ae
Gustavo <yecid.x.romero.-nd@disney.com>
2020-06-10 17:59:06 -0500

    Video 15 Getting input from users with process.argv

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,20 +2,12 @@ const chalk = require('chalk');
 const validator = require('validator');
 const getNotes = require('./notes.js');
 
-const notes = getNotes();
-const green = chalk.green.inverse.bold;
-const blue = chalk.blue.inverse.bold;
-const red = chalk.red.inverse.bold;
+const command = process.argv[2];
 
+console.log(process.argv);
 
-console.log(notes);
-console.log("isURL?", validator.isURL('https/mead.io'));
-console.log("isEmail?", validator.isEmail('bla@example.com'));
-console.log(`Welcome to ${red('Node JS')} with ${blue('Chalk')}!`);
-
-// Installing Nodemon in our App
-// Run npm i -g nodemon ----> to install globally
-// Instead node app.js ---> Use nodemon app.js
-// With nodemon you can save changes and without re-run the app see the changes directly
-
-// NODEMON WORKS WITHOUT RUN IT AGAIN
+if (command === 'add') {
+    console.log('Adding note!')
+} else if (command === 'remove') {
+    console.log('Removing note!')
+}
