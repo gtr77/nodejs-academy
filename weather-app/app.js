66f314c14a121c1ffe145e71e4bb8c9a34bab82d
Gustavo <yecid.x.romero.-nd@disney.com>
2020-06-16 20:41:54 -0500

    Video 33 Following a HTTP Request

--- weather-app/app.js
+++ weather-app/app.js
@@ -23,13 +23,16 @@
 // }
 const request = require("request");
 
-const url = 'http://api.weatherstack.com/current?access_key=22908e476b3aed230dfc802bb0be595f&query=37.8267,-122-4233&units=m';
+const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZ3RyNzdzcnYiLCJhIjoiY2tiaW83YXJsMGhjZTJ5bnpoNzZ0cnZlaCJ9.kX7pE6GNnNnStVswkE629g&limit=1';
 
 request({ url: url, json: true }, (error, response) => {
     // const data = JSON.parse(response.body);
     // console.log(data.current);
-    console.log(response.body.current);
-    console.log(response.body.current.weather_descriptions[0] + " It's currently " + response.body.current.temperature + " degress out. It feels like " + response.body.current.feelslike + " degrees out.")
+    const longitude = response.body.features[0].center[0];
+    const latitude = response.body.features[0].center[1];
+    console.log(longitude);
+    console.log(latitude);
+
 });
 
 
