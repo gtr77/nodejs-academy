b140766342b5cc347bc8b1094b28b714e5ae0baa
Gustavo <yecid.x.romero.-nd@disney.com>
2020-06-16 19:31:10 -0500

    Video 31 Creating the http request with request library

--- weather-app/app.js
+++ weather-app/app.js
@@ -21,34 +21,13 @@
 //         })
 //     })
 // }
-console.log("Starting");
+const request = require("request");
 
-setTimeout(() => {
-    console.log("2 Timer")
-},2000)
+const url = 'http://api.weatherstack.com/current?access_key=22908e476b3aed230dfc802bb0be595f&query=37.8267,-122-4233';
 
-setTimeout(() => {
-    console.log("0 Second Timer")
-},0)
-
-console.log("Stopping");
-
-/* KEY CONCEPTS
-CALL STACK -> THE PLACE THAT STARTS WITH MAIN() AND CALL EVERY SINGLE LINE OF THE PROGRAM
-    -> remove all logs and main from the callStack
-    -> Program Finishes
-    -> console.log("Stopping");
-    -> console.log("Starting");
-    -> main()
-NODE API'S -> ASYNCRONOUS CODE RUNNING BEHIND THE SCENES LIKE THE SETTIMEOUT FUNCTION
-    -> setTimeout 0 Seconds
-    -> setTimeout 2 Seconds
-    -> waiting to the callStack to be clean
-    -> add to the list
-CALLBACK QUEUE, -> THIS IS THE EVENT LOOP THAT ADD THE NODE OR API REQUEST TO BE READY TO EXECUTE
-    -> setTimeout 0 Seconds
-    -> setTimeout 2 Seconds
-    -> Clean Call Stack
- */
+request({ url: url}, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.current);
+});
 
 
