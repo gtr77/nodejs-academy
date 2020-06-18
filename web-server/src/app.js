a5c2a2d8f34b3a595c38fedc75c5d4a7a8b5d686
Gustavo <yecid.x.romero.-nd@disney.com>
2020-06-18 16:49:04 -0500

    Video 44 & 45 Serv Html and Json Format and Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,15 @@
+const path = require('path');
 const express = require('express');
 
+// console.log(__filename)
+// console.log(__dirname);
+// console.log(path.join(__dirname, '../public'));
+
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath));
+
 // res.render('index', {
 //     title: 'Weather',
 //     name: 'Andrew Mead'
@@ -8,22 +17,22 @@ const app = express();
 app.get('', (req, res) => {
     res.send("Hello Express");
 })
-app.get('/help', (req, res) => {
-    res.send("<h1>Help Page</h1>");
-})
+// app.get('/help', (req, res) => {
+//     res.send("<h1>Help Page</h1>");
+// })
 app.get('/challenge', (req, res) => {
     res.send("<h1>Challenge Page</h1>");
 })
-app.get('/about', (req, res) => {
-    res.send([
-        {
-            title: 'Weather'
-        },
-        {
-            name: 'Gustavo Romero'
-        }
-    ]);
-})
+// app.get('/about', (req, res) => {
+//     res.send([
+//         {
+//             title: 'Weather'
+//         },
+//         {
+//             name: 'Gustavo Romero'
+//         }
+//     ]);
+// })
 app.get('/weather', (req, res) => {
     res.send({
         forecast: "Its raining",
