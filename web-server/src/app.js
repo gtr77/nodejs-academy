6463a3f9808f3467c84c21a9522db9dc77a26c7a
Gustavo <yecid.x.romero.-nd@disney.com>
2020-06-18 17:08:29 -0500

    Video 46 Loading Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,6 @@
 const path = require('path');
 const express = require('express');
 
-// console.log(__filename)
-// console.log(__dirname);
-// console.log(path.join(__dirname, '../public'));
-
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public')
 
@@ -14,25 +10,7 @@ app.use(express.static(publicDirectoryPath));
 //     title: 'Weather',
 //     name: 'Andrew Mead'
 // })
-app.get('', (req, res) => {
-    res.send("Hello Express");
-})
-// app.get('/help', (req, res) => {
-//     res.send("<h1>Help Page</h1>");
-// })
-app.get('/challenge', (req, res) => {
-    res.send("<h1>Challenge Page</h1>");
-})
-// app.get('/about', (req, res) => {
-//     res.send([
-//         {
-//             title: 'Weather'
-//         },
-//         {
-//             name: 'Gustavo Romero'
-//         }
-//     ]);
-// })
+
 app.get('/weather', (req, res) => {
     res.send({
         forecast: "Its raining",
@@ -42,7 +20,3 @@ app.get('/weather', (req, res) => {
 app.listen(3000, () => {
     console.log('Server on 3000')
 })
-
-// app.com
-// app.com/help
-// app.com/about
