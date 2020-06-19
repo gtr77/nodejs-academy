de7d2b53a05e29228c6594bb93fdcc8f3f25dec5
Gustavo <yecid.x.romero.-nd@disney.com>
2020-06-19 13:38:28 -0500

    Video 49 Templating With Partials and add Some styles

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,22 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs')
 
 const app = express();
 
 // PATHS FOR EXPRESS CONFIG
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // SETUP HABDLEBARS ENGINE AND VIEWS LOCATION
 app.set('view engine', 'hbs');
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// RUN: nodemon src\app.js -e js,hbs
+// RUN: nodemon src\app.js -e js,hbs
+// RUN: nodemon src\app.js -e js,hbs
 
 // SETUP STATIC DIRECTORY
 app.use(express.static(publicDirectoryPath));
@@ -17,6 +24,7 @@ app.use(express.static(publicDirectoryPath));
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather App',
+        author: 'Beethoven',
         name: 'Gustavo Romero'
     });
 });
@@ -24,12 +32,14 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
+        author: 'Beethoven',
         description: 'Show something about you'
     });
 });
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'HELP!!!!',
+        title: 'Help',
+        author: 'Beethoven',
         description: 'Anybody can help me?'
     });
 });
