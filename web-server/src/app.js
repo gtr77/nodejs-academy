e89c11b8d25921610c72729dd05475f9ea0e93ef
Gustavo <yecid.x.romero.-nd@disney.com>
2020-06-19 11:48:46 -0500

    Video 47 Express Using HandleBars as Routing

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,29 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Gustavo Romero'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        description: 'Show something about you'
+    });
+});
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'HELP!!!!',
+        description: 'Anybody can help me?'
+    });
+});
+
 
 // res.render('index', {
 //     title: 'Weather',
