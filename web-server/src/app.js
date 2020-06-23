0864359c49cd04798ae9825bf6000a39287e2cc5
Gustavo <yecid.x.romero.-nd@disney.com>
2020-06-23 12:40:40 -0500

    Video 54 Creating Queries into URL's

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -45,9 +45,29 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address){
+        return res.send({
+            error: 'You must provide a address'
+        });
+    }
     res.send({
         forecast: "Its raining",
-        location: "Philadelphia"
+        location: "Philadelphia",
+        address: req.query.address
+    });
+});
+
+app.get('/products', (req, res) => {
+    if (!req.query.search){
+        return res.send({
+            error: 'You must provide a search term'
+        });
+    }
+
+    console.log(req.query.search);
+    res.send({
+        products: [],
+        address: ''
     });
 })
 app.get('/help/*', (req, res) => {
