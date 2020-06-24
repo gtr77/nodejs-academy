b84eef4924d06f3e03285813fcb1a44c3fc871d2
Gustavo <yecid.x.romero.-nd@disney.com>
2020-06-24 15:09:39 -0500

    Video 59 User Interface for Search

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,18 +2,24 @@ console.log('Client side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value;
 
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                return console.log(`Error Message: ${data.error}`);
+                messageOne.textContent = `Error Message: ${data.error}`;
+            } else {
+                messageOne.textContent = `Location: ${data.location}`;
+                messageTwo.textContent = `Forecast: ${data.forecast}`;
             }
-            console.log(`Show location: ${data.location}`);
-            console.log(`Show forecast: ${data.forecast}`);
         })
     })
 
