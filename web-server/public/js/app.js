295adfd62d4cff82a15ab2bc13bfb36edac88a88
Gustavo <yecid.x.romero.-nd@disney.com>
2020-06-24 14:07:45 -0500

    Video 58 Creating a Search From

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,20 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://localhost:3000/weather?address=bogota').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            return console.log(`Error Message: ${data.error}`);
-        }
-        console.log(`Show location: ${data.location}`);
-        console.log(`Show forecast: ${data.forecast}`);
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value;
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                return console.log(`Error Message: ${data.error}`);
+            }
+            console.log(`Show location: ${data.location}`);
+            console.log(`Show forecast: ${data.forecast}`);
+        })
     })
-})
+
+});
