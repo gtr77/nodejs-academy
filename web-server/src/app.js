f2dab7997af5e59d77305e0d8d56514435d9866d
Gustavo <yecid.x.romero.-nd@disney.com>
2020-06-24 11:23:38 -0500

    Video 56 Default Parameters ES6

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -52,7 +52,7 @@ app.get('/weather', (req, res) => {
             error: 'You must provide a address'
         });
     }
-    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
         if (error){
             return res.send({ error });
         }
