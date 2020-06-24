08e01984b73a59b697fdb529684ad0e1502173e2
Gustavo <yecid.x.romero.-nd@disney.com>
2020-06-24 13:46:21 -0500

    Video 57 Clean Code

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -9,20 +9,3 @@ fetch('http://localhost:3000/weather?address=bogota').then((response) => {
         console.log(`Show forecast: ${data.forecast}`);
     })
 })
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
-
-// fetch('http://localhost:3000/weather?address=!').then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//             console.log(data.error)
-//         } else {
-//             console.log(data.location)
-//             console.log(data.forecast)
-//         }
-//     })
-// })
