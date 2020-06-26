4b88cb71e75ae4b9e6ddb3d5a043b542e5b99ae1
gtr77 <gtr77srv@gmail.com>
2020-06-26 12:55:30 -0500

    Video 75 Testing MongoDB Connection

---
+++ task-manager/mongodb.js
@@ -0,0 +1,14 @@
+// CRUD
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!');
+    }
+    console.log('Sucessful Conection');
+});
