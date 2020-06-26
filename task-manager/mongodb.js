a64c4dd86f4162b3c87492f76811e077f934fa0a
gtr77 <gtr77srv@gmail.com>
2020-06-26 13:59:10 -0500

    Video 76 Inserting Data into MongoDB

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -10,5 +10,19 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     if (error) {
         return console.log('Unable to connect to database!');
     }
-    console.log('Sucessful Conection');
+    console.log('Successful Connection');
+    const db = client.db(databaseName);
+
+    db.collection('users').insertOne({
+        name: 'Gustavo',
+        age: 29
+    }, (error, result) => {
+        if (error){
+            return console.log('Unable to insert an user!');
+        }
+        // Show document or Object ready to be inserted in MongoDB
+        console.log(result.ops)
+    })
 });
+
+// node mongodb.js --> To test connection
