c725a0f2f9f861524cbec77d9bc960df2e5204a0
gtr77 <gtr77srv@gmail.com>
2020-07-06 10:42:54 -0500

    Video 91-92 Reading Endpoints and interact with PostMan!!

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,7 +7,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-//
+
 app.post('/users', (req, res) => {
     const user = new User(req.body)
 
@@ -18,6 +18,28 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const tasks = new Tasks(req.body)
 
@@ -25,6 +47,25 @@ app.post('/tasks', (req, res) => {
         res.send(tasks)
     }).catch((e) => {
         res.status(400).send(e)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Tasks.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+    Tasks.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
     })
 })
 
