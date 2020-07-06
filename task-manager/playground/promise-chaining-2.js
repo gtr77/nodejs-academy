f6594941aecf3a62da69aed55c1b1f62423492dd
gtr77 <gtr77srv@gmail.com>
2020-07-06 15:18:55 -0500

    Video 96 Async/Await Update and DeleteById Promises

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,11 +1,22 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/tasks')
 
-Task.findOneAndDelete('5efe2cffb213582d7014ee4c').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: true })
-}).then((result) => {
-    console.log(result)
+// Task.findOneAndDelete('5efe2cffb213582d7014ee4c').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: true })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({ completed: true })
+    return count;
+};
+deleteTaskAndCount('5efe2d10b213582d7014ee4d').then((results) => {
+    console.log(results)
 }).catch((e) => {
     console.log(e)
 })
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('5efe1ec3d2cc5024e0699d77', { age: 1 }).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 1 })
-}).then((result) => {
-    console.log(result)
+// User.findByIdAndUpdate('5efe1ec3d2cc5024e0699d77', { age: 1 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 1 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('5efe29305de97263d84774c1', 2).then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
