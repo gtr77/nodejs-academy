bf533d9da1ce9b40c50417100b0c59f0f480d035
gtr77 <gtr77srv@gmail.com>
2020-07-08 10:02:35 -0500

    Video 103 Storing Passwords

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -10,6 +10,20 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+const bcrypt = require('bcryptjs');
+
+const myFunction = async () => {
+    const password = 'SomePass165425432??!!???';
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    console.log(password)
+    console.log(hashedPassword)
+
+    const isMatch = await bcrypt.compare('SomePass165425432??!!???', hashedPassword)
+    console.log(isMatch)
+};
+
+myFunction()
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
